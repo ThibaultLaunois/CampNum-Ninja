78b1c410c0b7cfec920f6459d119644f475b2062
AMC <16638904+clarka34@users.noreply.github.com>
2025-12-17 12:15:40 +0100

    feat: class mediapipeProcessor V1

--- Ninja/mediapipeProcessor.py
+++ Ninja/mediapipeProcessor.py
@@ -6,102 +6,31 @@ import mediapipe as mp
 import numpy as np
 
 class mediapipeProcessor:
-    
 
     def __init__(self) -> None:
         Holistic = mp.solutions.holistic.Holistic
         self.model = Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5)
-        #self.PompeCount = PompeCount
-        self.camera = cv2.VideoCapture(0)
-        self.nameWindow = "Press q to exit"
-        cv2.namedWindow(self.nameWindow)
-        cv2.setMouseCallback(self.nameWindow, self.mouse_click)
-        self.windowWidth = 80
-        #Start Button
-        self.startButtonLocations = ((10, 10), (10 + self.windowWidth, 40))
-        #Pause Button
-        self.pauseButtonLocations = ((320 - self.windowWidth//2, 10), (320 + self.windowWidth//2, 40))
-        #Restart Button
-        self.restartButtonLocations = ((630 - self.windowWidth, 10), (630, 40))
 
-        self.counterOn = False
-        
 
-    def mouse_click(self, event, x, y, flags, param):
-        if event == cv2.EVENT_LBUTTONDOWN:
-            if (
-                x > self.startButtonLocations[0][0] and 
-                x < self.startButtonLocations[1][0] and 
-                y > self.startButtonLocations[0][1] and
-                y < self.startButtonLocations[1][1]
-            ):
-                self.counterOn = True
-
-        if event == cv2.EVENT_LBUTTONDOWN:
-            if (
-                x > self.startButtonLocations[0][0] and 
-                x < self.startButtonLocations[1][0] and 
-                y > self.startButtonLocations[0][1] and
-                y < self.startButtonLocations[1][1]
-            ):
-                self.counterOn = True
-
-    def drawInterface(self, image):
-        #start button
-        cv2.rectangle(image, self.startButtonLocations[0], self.startButtonLocations[1], (0,0,0), -1)
-        text = "Start"
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        font_scale = 1
-        color = (255, 0, 0)
-        thickness = 2
-        (w, h), _ = cv2.getTextSize(text, font, font_scale, thickness)
-        x_text = self.startButtonLocations[0][0] - w//2
-        y_text = (self.startButtonLocations[0][1] - h)//2
-        image = cv2.putText(image, text, (x_text, y_text), font, font_scale, color, thickness)
-        
-        #stop button
-        cv2.rectangle(image, self.pauseButtonLocations[0], self.pauseButtonLocations[1], (0,0,0), -1)
-
-        #reset button
-        cv2.rectangle(image, self.restartButtonLocations[0], self.restartButtonLocations[1], (0,0,0), -1)
-
-        if self.counterOn:
-                #execute step to count pomp
-                text = "Counter ON"
-                position = (200, 200)
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                font_scale = 1
-                color = (255, 0, 0)
-                thickness = 2
-                image = cv2.putText(image, text, position, font, font_scale, color, thickness)
-    
-    def get_pose_world_landmarks(self, image):
+    def get_body_landmarks(self, image):
         results = self.model.process(image) 
-        return results.pose_world_landmarks
+        return results.pose_landmarks
     
-    def show_interface(self):
-        while self.camera.isOpened():
-            #count_pompe = self.PompeCount.getPompeCount
-            _, image = self.camera.read()
-            image = cv2.flip(image, 1)
-            
-            #Compute things
-            
-            self.drawInterface(image)
-            cv2.imshow(self.nameWindow, image)
 
-
-            if cv2.waitKey(5) & 0xFF == ord('q'):
-                    break
-            
-        self.camera.release()
-        cv2.destroyAllWindows()
-
+    def get_right_hand_landmarks(self, image):
+        results = self.model.process(image) 
+        return results.right_hand_landmarks
     
-    def format_landmarks(self, world_landmarks):
+
+    def get_left_hand_landmarks(self, image):
+        results = self.model.process(image) 
+        return results.left_hand_landmarks
+    
+
+    def format_landmarks(self, landmarks_in):
         """
-        Transforms the output of `get_pose_world_landmarks()` for a given imagefrom LandmarkList object to 
-        a dictionnary containing the index of landmarks, and its corresponding coordinates 
+        Transforms landmarks obtained from an image to a dictionary containing the index of 
+        the landmarks and their corresponding coordinates.
         """
         landmarks = {
             "index": [],
@@ -110,7 +39,7 @@ class mediapipeProcessor:
             "Z": []
         }
 
-        for i, lm in enumerate(world_landmarks.landmark[:]):
+        for i, lm in enumerate(landmarks_in.landmark[:]):
             landmarks["index"].append(i)
             landmarks["X"].append(lm.x)
             landmarks["Y"].append(lm.y)
@@ -118,7 +47,8 @@ class mediapipeProcessor:
 
         return landmarks
     
-    def get_landmarks_from_folder(self, source_path, target_path):
+
+    def get_hand_landmarks_from_folder(self, source_path, target_path):
         """
         
         """
@@ -130,18 +60,25 @@ class mediapipeProcessor:
             filename = f"{source_path}/{file}"
             try:
                 image = cv2.imread(filename)
-                world_landmarks = self.get_pose_world_landmarks(image)
-                landmarks = self.format_landmarks(world_landmarks)
-                data["filename"].append(filename)
-                data["landmarks"].append(landmarks)
+                right_hand = self.get_right_hand_landmarks(image)
+                left_hand = self.get_left_hand_landmarks(image)
+                if right_hand:
+                    right_hand = self.format_landmarks(right_hand)
+                    data["filename"].append(filename)
+                    data["landmarks"].append(right_hand)
+                if left_hand:
+                    left_hand = self.format_landmarks(left_hand)
+                    data["filename"].append(filename)
+                    data["landmarks"].append(left_hand)
             except Exception as e:
                 with open(f"{target_path}/log.txt", "a") as f:
                     f.write(f"{filename}\n")
                     f.write(f"{e}\n")
 
-        with open(f'{target_path}/landmarks.pickle', 'wb') as handle:
+        with open(f'{target_path}/hand_landmarks.pickle', 'wb') as handle:
             pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
             
+
     def landmarks_to_array(self, landmarks):
         res = np.zeros(shape=(33,3))
         res[:,0] = landmarks["X"]
