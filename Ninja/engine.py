d230f74583bd83b69e85321663201fa8bf5a6326
Julien Dubois <julienhm.dubois@gmail.com>
2025-12-17 11:42:23 +0100

    chore: init class Engine

---
+++ Ninja/engine.py
@@ -0,0 +1,21 @@
+from game import Game
+from interface import Interface
+
+class Engine:
+    """
+    
+    """
+    def __init__(self, game:Game, interface:Interface):
+        self.game = Game
+        self.interface = interface
+        self.gameOn = False
+        self.objects = []
+    
+    def startGame(self):
+        pass
+
+    def endGame(self):
+        pass
+
+    def updateInterface(self):
+        pass
