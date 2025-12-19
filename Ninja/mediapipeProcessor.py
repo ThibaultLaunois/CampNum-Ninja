bf08048f8bc1979fb0164927e7fde403d8894e1a
AMC <16638904+clarka34@users.noreply.github.com>
2025-12-19 11:07:04 +0100

    chore: changing model parameters

--- Ninja/mediapipeProcessor.py
+++ Ninja/mediapipeProcessor.py
@@ -11,10 +11,10 @@ class mediapipeProcessor:
         # HandLandmarkerOptions (details here: https://developers.google.com/mediapipe/solutions/vision/hand_landmarker/python#live-stream)
         options = mp.tasks.vision.HandLandmarkerOptions( 
             base_options = mp.tasks.BaseOptions(model_asset_path="Ninja/hand_landmarker.task"), # path to model
-            num_hands = 2, # track both hands
-            min_hand_detection_confidence = 0.3, # lower than value to get predictions more often
-            min_hand_presence_confidence = 0.3, # lower than value to get predictions more often
-            min_tracking_confidence = 0.3)
+            num_hands = 10, # track both hands
+            min_hand_detection_confidence = 0.5, # lower than value to get predictions more often
+            min_hand_presence_confidence = 0.5, # lower than value to get predictions more often
+            min_tracking_confidence = 0.5)
         
         # initialize landmarker
         self.landmarker = self.landmarker.create_from_options(options)
